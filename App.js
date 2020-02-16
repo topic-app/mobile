abbd4c9b826676fdb387bba57a33d7e007c5c551
Alexander N <alex@nitters.eu>
2020-02-16 17:50:06 +0100

    Add redux package and use provider structure

--- App.js
+++ App.js
@@ -2,17 +2,42 @@
 import React, { Component } from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { Provider as PaperProvider, DefaultTheme } from 'react-native-paper';
 
 import DrawerNavigator from './src/views/main/index';
 import SettingsNavigator from './src/views/settings/index';
+import AuthNavigator from './src/views/auth/index';
+import LocationNavigator from './src/views/location/index';
 
 const RootNavigator = createStackNavigator({
   Main: DrawerNavigator,
   Settings: SettingsNavigator,
+  Auth: AuthNavigator,
+  Location: LocationNavigator,
 }, {
   headerMode: 'none',
 });
 
-const App = createAppContainer(RootNavigator);
+const theme = {
+  ...DefaultTheme,
+  roundness: 2,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#3498db',
+    accent: '#f1c40f',
+  },
+};
 
-export default App;
+
+const Navigation = createAppContainer(RootNavigator);
+
+// Render the app container component with the provider around it
+export default class App extends React.Component {
+  render() {
+    return (
+      <PaperProvider theme={theme}>
+        <Navigation />
+      </PaperProvider>
+    );
+  }
+}
