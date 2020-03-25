991f39a9584079cd28623b37ffaf1604be61ba25
Alexander N <alex@nitters.eu>
2020-03-25 18:20:43 +0100

    Remove useless web stuff

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-import { AppRegistry, Platform } from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
 
 AppRegistry.registerComponent('Topic', () => App);
-
-if (Platform.OS === 'web') {
-  const rootTag = document.getElementById('root') || document.getElementById('main');
-  AppRegistry.runApplication('Topic', { rootTag });
-}
