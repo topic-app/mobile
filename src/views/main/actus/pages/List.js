66b1464fa59cf7aead647bc7094133e36797af75
Mega-Volt <megavoltyt@gmail.com>
2020-03-25 12:22:50 +0100

    Finish converting classes to functions

--- src/views/main/actus/pages/List.js
+++ src/views/main/actus/pages/List.js
@@ -1,47 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { View, FlatList } from 'react-native';
-import { Button, withTheme } from 'react-native-paper';
+import { Button } from 'react-native-paper';
 
 import ActuComponentListCard from '../components/listCard';
 
 import data from '../data/testDataList.json';
 import { styles } from '../../../../styles/Styles';
 
-class ActuListScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Actus et évènements',
-  };
-
-  render() {
-    const { navigation } = this.props;
-    const { navigate } = navigation;
-
-    return (
-      <View style={styles.page}>
-        <FlatList
-          data={data}
-          refreshing={false}
-          onRefresh={() => console.log('Refresh')}
-          keyExtractor={(article) => article.articleId}
-          ListFooterComponent={(
-            <View style={styles.container}>
-              <Button style={styles.text}>Retour en haut</Button>
-            </View>
-          )}
-          renderItem={(article) => (
-            <ActuComponentListCard
-              article={article.item}
-              navigate={() => navigate('ActuArticle', { id: article.item.articleId })}
-            />
-          )}
-        />
-      </View>
-    );
-  }
+function ActuListScreen({ navigation }) {
+  return (
+    <View style={styles.page}>
+      <FlatList
+        data={data}
+        refreshing={false}
+        onRefresh={() => console.log('Refresh')}
+        keyExtractor={(article) => article.articleId}
+        ListFooterComponent={(
+          <View style={styles.container}>
+            <Button style={styles.text}>Retour en haut</Button>
+          </View>
+        )}
+        renderItem={(article) => (
+          <ActuComponentListCard
+            article={article.item}
+            navigate={() => navigation.navigate('ActuArticle', { id: article.item.articleId })}
+          />
+        )}
+      />
+    </View>
+  );
 }
 
-export default withTheme(ActuListScreen);
+export default ActuListScreen;
 
 ActuListScreen.propTypes = {
   navigation: PropTypes.shape({
