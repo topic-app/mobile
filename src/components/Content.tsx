17d8f6f1585d2e4a232c27270da3c75318243716
Benjamin Sengupta <benjaminsengupta@gmail.com>
2020-06-14 16:07:47 +0200

    fix: remove markdown-it from package.json

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { Text, withTheme } from 'react-native-paper';
 import PropTypes from 'prop-types';
-import Markdown from 'react-native-markdown-display';
-import MarkdownIt from 'markdown-it';
+import Markdown, { MarkdownIt } from 'react-native-markdown-display';
 import FitImage from 'react-native-fit-image';
 import { config } from '@root/app.json';
 import handleUrl from '@utils/handleUrl';
