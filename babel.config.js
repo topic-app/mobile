ba440c9e27355db1a5e657a0c7a90ce58a82bbf7
Benjamin Sengupta <benjaminsengupta@gmail.com>
2020-06-14 14:45:10 +0200

    fix: correct most eslint errors

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,38 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@root': '.',
+          '@src': './src',
+          '@components': './src/components',
+          '@assets': './src/assets',
+          '@redux': './src/redux',
+          '@styles': './src/styles',
+          '@utils': './src/utils',
+        },
+        extensions: [
+          '.js',
+          '.android.js',
+          '.ios.js',
+          '.native.js',
+          '.jsx',
+          '.android.jsx',
+          '.ios.jsx',
+          '.ts',
+          '.tsx',
+          '.ios.tsx',
+          '.android.tsx',
+        ],
+      },
+    ],
+  ],
+  env: {
+    production: {
+      plugins: ['react-native-paper/babel'],
+    },
+  },
 };
