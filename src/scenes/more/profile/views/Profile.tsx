ef1041133af9e16bab4743fbd606dc80e56ae83b
Jeremy_I8 <jeremyhendrikse@gmail.com>
2020-07-15 22:20:29 +0200

    profile avancé mais pas finit du tout

--- src/scenes/more/profile/views/Profile.tsx
+++ src/scenes/more/profile/views/Profile.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { View, ScrollView } from 'react-native';
-import { Text, useTheme } from 'react-native-paper';
+import { Text, useTheme, Title, Subheading, Divider } from 'react-native-paper';
 import { connect } from 'react-redux';
-
+import Avatar from '@components/Avatar';
 import getStyles from '@styles/Styles';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { TranslucentStatusBar } from '@root/src/components/Header.ios';
+import { InlineCard } from '@components/Cards';
 
 const account = {
   loggedIn: true,
@@ -30,7 +33,7 @@ const account = {
             angle: 23,
           },
         },
-        official: false,
+        official: true,
       },
       data: {
         // This data can only be shown if public is true, and can be directly modified by user
@@ -62,7 +65,20 @@ const account = {
           events: [],
         },
         location: {
-          schools: ['5e9208118851c52a8e6c6f8d'],
+          schools: [
+            {
+              _id: '5e9208118851c52a8e6c6f8d',
+              displayName: 'CIV',
+              address: {
+                address: {
+                  number: '3',
+                  street: 'chemin de chose',
+                  city: 'Valbonne',
+                  code: '3533',
+                },
+              },
+            },
+          ],
           departments: [],
           global: false,
         },
@@ -79,6 +95,22 @@ const account = {
   },
 };
 
+function getAddressString(address) {
+  const { number, street, city, code } = address || {};
+  if (number && street && city && code) {
+    return `${number} ${street}, ${code} ${city}`;
+  }
+  if (city) return city;
+  return null;
+}
+
+function genName({ data, info }) {
+  if (data.firstName && data.lastName) {
+    return `${data.firstName} ${data.lastName}`;
+  }
+  return data.firstName || data.lastName || info.username;
+}
+
 function Profile() {
   const theme = useTheme();
   const styles = getStyles(theme);
@@ -86,6 +118,70 @@ function Profile() {
   return (
     <View style={styles.page}>
       <ScrollView>
+        <View style={[styles.contentContainer, { marginTop: 20, flexDirection: 'row' }]}>
+          <View>
+            <Avatar avatar={account.accountInfo.user.info.avatar} />
+          </View>
+          <View style={[styles.centerIllustrationContainer, { flexDirection: 'row' }]}>
+            {account.accountInfo.user.data.public ? (
+              <View>
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                  <Title>{genName(account.accountInfo.user)}</Title>
+                  <View style={{ marginLeft: 5 }}>
+                    {account.accountInfo.user.info.official && (
+                      <Icon name="check-decagram" color={colors.primary} size={20} />
+                    )}
+                  </View>
+                </View>
+                <Subheading style={{ marginTop: -10 }}>
+                  @{account.accountInfo.user.info.username}
+                </Subheading>
+              </View>
+            ) : (
+              <Title>@{account.accountInfo.user.info.username}</Title>
+            )}
+          </View>
+        </View>
+        <View style={{ flexDirection: 'row' }}>
+          <View>
+            <Text>{account.accountInfo.user.data.cache.followers}</Text>
+          </View>
+          <View>
+            <Text>
+              {account.accountInfo.user.data.following.groups.length +
+                account.accountInfo.user.data.following.users.length +
+                account.accountInfo.user.data.following.events.length}
+            </Text>
+          </View>
+        </View>
+        <View>
+          {account.accountInfo.user.data.location.global && (
+            <InlineCard
+              icon="map-marker"
+              title="France Entière"
+              onPress={() => console.log('global pressed')}
+            />
+          )}
+          {account.accountInfo.user.data.location.schools?.map((school) => (
+            <InlineCard
+              key={school._id}
+              icon="school"
+              title={school.displayName}
+              subtitle={getAddressString(school.address.address) || school.shortName}
+              onPress={() => console.log(`school ${school._id} pressed!`)}
+            />
+          ))}
+          {account.accountInfo.user.data.location.departments?.map((dep) => (
+            <InlineCard
+              key={dep._id}
+              icon="domain"
+              title={dep.displayName}
+              subtitle="Département"
+              onPress={() => console.log(`department ${dep._id} pressed!`)}
+            />
+          ))}
+        </View>
+
         <Text>Profile!</Text>
         <Text>{JSON.stringify(account, null, 2)}</Text>
       </ScrollView>
