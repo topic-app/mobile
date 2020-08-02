da10d3a06f4d18df86d08579f8a1517649e7aaf1
Benjamin Sengupta <benjaminsengupta@gmail.com>
2020-08-02 15:43:45 +0200

    feat(articles): add vertical padding to cards

--- src/components/cards/Article.tsx
+++ src/components/cards/Article.tsx
@@ -29,34 +29,32 @@ const ArticleCard: React.FC<Props> = ({ article, navigate, unread }) => {
   const readStyle = !unread && { color: colors.disabled };
 
   return (
-    <CardBase onPress={navigate} contentContainerStyle={{ paddingTop: 0, paddingBottom: 0 }}>
-      <View style={{ marginVertical: 5 }}>
-        <Card.Content>
-          <Title numberOfLines={2} style={readStyle}>
-            {article?.title}
-          </Title>
-          <Caption>{`Publié ${moment(article?.date).fromNow()}`}</Caption>
-          <View style={{ flexDirection: 'row', paddingTop: 6 }}>
-            <Image
-              source={{ uri: getImageUrl({ image: article?.image, size: 'small' }) }}
-              style={[styles.thumbnail, { width: imageSize, height: imageSize }]}
-            />
-            <View
-              style={{
-                marginLeft: 15,
-                flex: 1,
-              }}
-            >
-              <Paragraph numberOfLines={6} style={readStyle}>
-                {article?.summary}
-              </Paragraph>
-            </View>
+    <CardBase onPress={navigate}>
+      <Card.Content>
+        <Title numberOfLines={2} style={readStyle}>
+          {article?.title}
+        </Title>
+        <Caption>{`Publié ${moment(article?.date).fromNow()}`}</Caption>
+        <View style={{ flexDirection: 'row', paddingTop: 6 }}>
+          <Image
+            source={{ uri: getImageUrl({ image: article?.image, size: 'small' }) }}
+            style={[styles.thumbnail, { width: imageSize, height: imageSize }]}
+          />
+          <View
+            style={{
+              marginLeft: 15,
+              flex: 1,
+            }}
+          >
+            <Paragraph numberOfLines={6} style={readStyle}>
+              {article?.summary}
+            </Paragraph>
           </View>
-        </Card.Content>
-        <Card.Content style={{ marginTop: 5, paddingHorizontal: 0 }}>
-          <TagList item={article} scrollable={false} />
-        </Card.Content>
-      </View>
+        </View>
+      </Card.Content>
+      <Card.Content style={{ marginTop: 5, paddingHorizontal: 0 }}>
+        <TagList item={article} scrollable={false} />
+      </Card.Content>
     </CardBase>
   );
 };
