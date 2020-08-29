1d273928a1cc19d7466313acdf4f380803f1d5f2
Alexander Nitters <alex@nitters.eu>
2020-08-29 15:59:54 +0200

    fix(web): Make pressable work

--- src/components/DesktopModals.tsx
+++ src/components/DesktopModals.tsx
@@ -1,4 +1,7 @@
-const BottomModal = () => null;
+import React from 'react';
+import { Text } from 'react-native';
+
+const BottomModal = () => <Text>Test</Text>;
 const SlideAnimation = () => null;
 // TODO: Find something for desktop modals
 export { BottomModal, SlideAnimation };
