191863216453a45646309eadb016c7d0e420cbd3
Benjamin Sengupta <benjaminsengupta@gmail.com>
2020-10-02 15:27:45 +0200

    feat: add transparent android navbar, update deprecated safe area api, clean up code

--- android/app/src/main/java/fr/topicapp/topic/MainActivity.java
+++ android/app/src/main/java/fr/topicapp/topic/MainActivity.java
@@ -1,6 +1,7 @@
 package fr.topicapp.topic;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -17,6 +18,9 @@ public class MainActivity extends ReactActivity {
     // SplashScreen.show(...) has to be called after super.onCreate(...)
     // Below line is handled by '@expo/configure-splash-screen' command and it's discouraged to modify it manually
     SplashScreen.show(this, SplashScreenImageResizeMode.CONTAIN, false);
+
+    // set navigation bar to transparent
+    getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
   }
 
 
