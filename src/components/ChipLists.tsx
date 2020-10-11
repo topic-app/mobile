c074579c3ef8cf2b3a3130bf8728744c857ab3fc
Alexander Nitters <alex@nitters.eu>
2020-10-11 11:36:54 +0200

    fix(articles): Allow writing comments as group

--- src/components/ChipLists.tsx
+++ src/components/ChipLists.tsx
@@ -1,11 +1,20 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
-import { View, Platform, Animated, TextInput, FlatList, ViewStyle, StyleProp } from 'react-native';
+import {
+  View,
+  Platform,
+  Animated,
+  TextInput,
+  FlatList,
+  ViewStyle,
+  StyleProp,
+  TouchableOpacity,
+} from 'react-native';
 import { Text, IconButton, useTheme } from 'react-native-paper';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import color from 'color';
 
-import { PlatformTouchable } from './PlatformComponents';
+import { PlatformTouchable } from '@components/index';
 
 type ChipBaseProps = {
   children: React.ReactNode;
@@ -65,7 +74,7 @@ const ChipBase: React.FC<ChipBaseProps> = ({
         containerStyle,
       ]}
     >
-      <PlatformTouchable
+      <TouchableOpacity
         disabled={!onPress || rightAction}
         onPress={onPress}
         onPressIn={handlePressIn}
@@ -93,7 +102,7 @@ const ChipBase: React.FC<ChipBaseProps> = ({
             />
           )}
         </View>
-      </PlatformTouchable>
+      </TouchableOpacity>
     </Animated.View>
   );
 };
