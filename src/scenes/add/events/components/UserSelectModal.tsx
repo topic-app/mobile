07aa7946fce4ef3bb28e16c53780937836a45977
Alexander Nitters <alex@nitters.eu>
2020-11-07 18:49:02 +0100

    fix(events): Use new modals

--- src/scenes/add/events/components/UserSelectModal.tsx
+++ src/scenes/add/events/components/UserSelectModal.tsx
@@ -30,7 +30,7 @@ import { View, Platform, FlatList } from 'react-native';
 import Illustration from '@components/Illustration';
 import Avatar from '@components/Avatar';
 import { connect } from 'react-redux';
-import Modal, { BottomModal, SlideAnimation } from '@components/Modals';
+import { Modal } from '@components/index';
 import { searchTags, updateTags } from '@redux/actions/api/tags';
 import { searchGroups, updateGroups } from '@redux/actions/api/groups';
 import { searchUsers, updateUsers } from '@redux/actions/api/users';
@@ -75,89 +75,64 @@ function UserSelectModal({
   const data = searchText === '' ? account.accountInfo?.user?.data?.following?.users : users.search;
 
   return (
-    <BottomModal
-      visible={visible}
-      onTouchOutside={() => {
-        setVisible(false);
-      }}
-      onHardwareBackPress={() => {
-        setVisible(false);
-        return true;
-      }}
-      onSwipeOut={() => {
-        setVisible(false);
-      }}
-      modalAnimation={
-        new SlideAnimation({
-          slideFrom: 'bottom',
-          useNativeDriver: false,
-        })
-      }
-    >
-      <ThemeProvider theme={theme}>
-        <Card style={styles.modalCard}>
-          <View>
-            <View style={{ height: 200 }}>
-              <View style={styles.centerIllustrationContainer}>
-                <Illustration name="search" height={200} width={200} />
-              </View>
-            </View>
-            <Divider />
-            {state.search?.loading.initial && (
-              <ProgressBar indeterminate style={{ marginTop: -4 }} />
-            )}
-            {searchText !== '' && state.search?.error ? (
-              <ErrorMessage
-                type="axios"
-                strings={{
-                  what: 'la récupération des utilisateurs',
-                  contentPlural: 'des utilisateurs',
-                  contentSingular: "La liste d'utilisateurs",
-                }}
-                error={state.add?.error}
-                retry={() => update()}
-              />
-            ) : null}
-
-            <View style={styles.container}>
-              <Searchbar
-                autoFocus
-                placeholder="Rechercher"
-                value={searchText}
-                onChangeText={setSearchText}
-                onIdle={update}
-              />
-            </View>
-            <FlatList
-              data={data}
-              keyExtractor={(i) => i._id}
-              keyboardShouldPersistTaps="handled"
-              ListEmptyComponent={() => (
-                <View style={{ minHeight: 50 }}>
-                  {(searchText === '' && state.list.success) ||
-                    (searchText !== '' && state.search?.success && (
-                      <View style={styles.centerIllustrationContainer}>
-                        <Text>Aucun résultat</Text>
-                      </View>
-                    ))}
-                </View>
-              )}
-              renderItem={({ item }) => (
-                <List.Item
-                  title={`@${item.info?.username || item.displayName}`}
-                  left={() => <Avatar avatar={item.info?.avatar} size={50} />}
-                  onPress={() => {
-                          next(item);
-                          setVisible(false);
-                        }
-                  }
-                />
-              )}
-            />
+    <Modal visible={visible} setVisible={setVisible}>
+      <View>
+        <View style={{ height: 200 }}>
+          <View style={styles.centerIllustrationContainer}>
+            <Illustration name="search" height={200} width={200} />
           </View>
-        </Card>
-      </ThemeProvider>
-    </BottomModal>
+        </View>
+        <Divider />
+        {state.search?.loading.initial && <ProgressBar indeterminate style={{ marginTop: -4 }} />}
+        {searchText !== '' && state.search?.error ? (
+          <ErrorMessage
+            type="axios"
+            strings={{
+              what: 'la récupération des utilisateurs',
+              contentPlural: 'des utilisateurs',
+              contentSingular: "La liste d'utilisateurs",
+            }}
+            error={state.add?.error}
+            retry={() => update()}
+          />
+        ) : null}
+
+        <View style={styles.container}>
+          <Searchbar
+            autoFocus
+            placeholder="Rechercher"
+            value={searchText}
+            onChangeText={setSearchText}
+            onIdle={update}
+          />
+        </View>
+        <FlatList
+          data={data}
+          keyExtractor={(i) => i._id}
+          keyboardShouldPersistTaps="handled"
+          ListEmptyComponent={() => (
+            <View style={{ minHeight: 50 }}>
+              {(searchText === '' && state.list.success) ||
+                (searchText !== '' && state.search?.success && (
+                  <View style={styles.centerIllustrationContainer}>
+                    <Text>Aucun résultat</Text>
+                  </View>
+                ))}
+            </View>
+          )}
+          renderItem={({ item }) => (
+            <List.Item
+              title={`@${item.info?.username || item.displayName}`}
+              left={() => <Avatar avatar={item.info?.avatar} size={50} />}
+              onPress={() => {
+                next(item);
+                setVisible(false);
+              }}
+            />
+          )}
+        />
+      </View>
+    </Modal>
   );
 }
 
