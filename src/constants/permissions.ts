df6d646587e38d2cad443c212b74b848fe06f99f
Alexander Nitters <alex@nitters.eu>
2020-11-11 14:45:44 +0100

    fix(permissions): Update groupe permissions

--- src/constants/permissions.ts
+++ src/constants/permissions.ts
@@ -1,22 +1,18 @@
 export const ARTICLE_ADD = 'article.add';
 export const ARTICLE_MODIFY = 'article.modify';
 export const ARTICLE_DELETE = 'article.delete';
-export const ARTICLE_DELETE_IMMEDIATE = 'article.delete.immediate';
 export const ARTICLE_VERIFICATION_VIEW = 'article.verification.view';
 export const ARTICLE_VERIFICATION_APPROVE = 'article.verification.approve';
+export const ARTICLE_VERIFICATION_APPROVE_EXTRA = 'article.verification.approve.extra';
 
 export const COMMENT_ADD = 'commment.add';
 export const COMMENT_MODIFY = 'comment.modify';
 export const COMMENT_DELETE = 'comment.delete';
-export const COMMENT_DELETE_IMMEDIATE = 'comment.delete.immediate';
 
-export const GROUP_ADD = 'group.add';
 export const GROUP_MODIFY = 'group.modify';
 export const GROUP_MEMBERS_ADD = 'group.members.add';
 export const GROUP_MEMBERS_MODIFY = 'group.members.modify';
 export const GROUP_MEMBERS_DELETE = 'group.members.delete';
-export const GROUP_MEMBERSHIP_ACCEPT = 'membership.accept';
-export const GROUP_MEMBERSHIP_REJECT = 'membership.reject';
 export const GROUP_ROLES_ADD = 'group.roles.add';
 export const GROUP_ROLES_MODIFY = 'group.roles.modify';
 export const GROUP_ROLES_DELETE = 'group.roles.delete';
@@ -24,12 +20,12 @@ export const GROUP_VERIFICATION_VIEW = 'group.verification.view';
 export const GROUP_VERIFICATION_APPROVE = 'group.verification.approve';
 
 export const PETITION_ADD = 'petition.add';
-export const PETITION_MODIFY = 'petition.modify';
 export const PETITION_MESSAGE_ADD = 'petition.message.add';
 export const PETITION_DELETE = 'petition.delete';
 export const PETITION_DELETE_IMMEDIATE = 'petition.delete.immediate';
 export const PETITION_VERIFICATION_VIEW = 'petition.verification.view';
 export const PETITION_VERIFICATION_APPROVE = 'petition.verification.approve';
+export const PETITION_VERIFICATION_APPROVE_EXTRA = 'petition.verification.approve.extra';
 
 export const EVENT_ADD = 'event.add';
 export const EVENT_MODIFY = 'event.modify';
@@ -37,12 +33,14 @@ export const EVENT_DELETE = 'event.delete';
 export const EVENT_DELETE_IMMEDIATE = 'event.delete.immediate';
 export const EVENT_VERIFICATION_VIEW = 'event.verification.view';
 export const EVENT_VERIFICATION_APPROVE = 'event.verification.approve';
+export const EVENT_VERIFICATION_APPROVE_EXTRA = 'event.verification.approve.extra';
 
-// export const PLACE_ADD = 'place.add';
-// export const PLACE_MODIFY = 'place.modify';
-// export const PLACE_VERIFICATION_VIEW = 'place.verification.view';
-// export const PLACE_VERIFICATION_APPROVE = 'place.verification.approve';
+export const PLACE_ADD = 'place.add';
+export const PLACE_MODIFY = 'place.modify';
+export const PLACE_DELETE = 'place.delete';
+export const PLACE_VERIFICATION_VIEW = 'place.verification.view';
+export const PLACE_VERIFICATION_APPROVE = 'place.verification.approve';
+export const PLACE_VERIFICATION_APPROVE_EXTRA = 'place.verification.approve.extra';
 
 export const TAG_ADD = 'tag.add';
 export const TAG_DELETE = 'tag.delete';
-export const TAG_DELETE_IMMEDIATE = 'tag.delete.immediate';
