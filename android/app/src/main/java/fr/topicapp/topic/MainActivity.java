ea23aaffcebd9c620886ed8cf90c632de615aa10
Benjamin Sengupta <benjaminsengupta@gmail.com>
2020-11-23 10:49:18 +0100

    chore(npm): upgrade expo dependencies, fix android overview insets

--- android/app/src/main/java/fr/topicapp/topic/MainActivity.java
+++ android/app/src/main/java/fr/topicapp/topic/MainActivity.java
@@ -13,54 +13,57 @@ import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
-import expo.modules.splashscreen.SplashScreen;
+import expo.modules.splashscreen.singletons.SplashScreen;
 import expo.modules.splashscreen.SplashScreenImageResizeMode;
 
 public class MainActivity extends ReactActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    // SplashScreen.show(...) has to be called after super.onCreate(...)
-    // Below line is handled by '@expo/configure-splash-screen' command and it's discouraged to modify it manually
-    SplashScreen.show(this, SplashScreenImageResizeMode.CONTAIN, true);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // SplashScreen.show(...) has to be called after super.onCreate(...)
+        // Below line is handled by '@expo/configure-splash-screen' command and it's
+        // discouraged to modify it manually
+        SplashScreen.show(this, SplashScreenImageResizeMode.CONTAIN, ReactRootView.class, true);
 
-    // Set Android Navigation Bar to transparent if device is using fullscreen gestures
-    int isEdgeToEdgeEnabled = 0;
-    Resources resources = this.getResources();
-    int resourceId = resources.getIdentifier("config_navBarInteractionMode", "integer", "android");
-    if (resourceId > 0) {
-        isEdgeToEdgeEnabled = resources.getInteger(resourceId);
+        // Set Android Navigation Bar to transparent if device is using fullscreen
+        // gestures
+        int isEdgeToEdgeEnabled = 0;
+        Resources resources = this.getResources();
+        int resourceId = resources.getIdentifier("config_navBarInteractionMode", "integer", "android");
+        if (resourceId > 0) {
+            isEdgeToEdgeEnabled = resources.getInteger(resourceId);
+        }
+
+        Window window = getWindow();
+        View decorView = window.getDecorView();
+
+        // Check if device is using fullscreen gestures
+        if (isEdgeToEdgeEnabled == 2) {
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.setNavigationBarColor(Color.TRANSPARENT);
+        } else {
+            // Device is using 2-button or 3-button navigation
+            // Change Android Navigation Buttons to correct theme configuration
+            int flags = decorView.getSystemUiVisibility();
+            switch (getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK) {
+                case Configuration.UI_MODE_NIGHT_YES:
+                    window.setNavigationBarColor(0xff000000);
+                    flags &= ~View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR;
+                    break;
+                case Configuration.UI_MODE_NIGHT_NO:
+                    window.setNavigationBarColor(0xffffffff);
+                    // window.setNavigationBarDividerColor(0xffefefef);
+                    flags |= View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR;
+                    break;
+            }
+            decorView.setSystemUiVisibility(flags);
+        }
     }
-
-    Window window = getWindow();
-
-    // Check if device is using fullscreen gestures
-    if (isEdgeToEdgeEnabled == 2) {
-        window.setFlags(LayoutParams.FLAG_LAYOUT_NO_LIMITS, LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-        window.setNavigationBarColor(Color.TRANSPARENT);
-    } else {
-      // Device is using 2-button or 3-button navigation
-      // Change Android Navigation Buttons to correct theme configuration
-      View decorView = window.getDecorView();
-      int flags = decorView.getSystemUiVisibility();
-      switch (getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK) {
-          case Configuration.UI_MODE_NIGHT_YES:
-              window.setNavigationBarColor(0xff000000);
-              flags &= ~View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR;
-              break;
-          case Configuration.UI_MODE_NIGHT_NO:
-              window.setNavigationBarColor(0xffffffff);
-              // window.setNavigationBarDividerColor(0xffefefef);
-              flags |= View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR;
-              break; 
-      }
-      decorView.setSystemUiVisibility(flags);
-    }
-  }
 
     /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
+     * Returns the name of the main component registered from JavaScript. This is
+     * used to schedule rendering of the component.
      */
     @Override
     protected String getMainComponentName() {
