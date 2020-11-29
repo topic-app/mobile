2103d9b5ed128c749b6cc328d0d819cb5a7bc4fc
Alexander Nitters <alex@nitters.eu>
2020-11-29 12:46:35 +0100

    ts: Start improving typings in scenes/configure

--- src/scenes/configure/components/QuickSelectModal.tsx
+++ src/scenes/configure/components/QuickSelectModal.tsx
@@ -19,13 +19,9 @@ import {
   TagsState,
   GroupsState,
   UsersState,
-  Tag,
-  Group,
   User,
-  RequestState,
   DepartmentsState,
   SchoolsState,
-  ArticleListItem,
   EventQuickItem,
   TagPreload,
   UserPreload,
@@ -45,7 +41,7 @@ import getArticleStyles from './styles/Styles';
 type QuickSelectModalProps = ModalProps & {
   articleQuicks: ArticleQuickItem[];
   eventQuicks: EventQuickItem[];
-  dataType: string;
+  dataType: 'tag' | 'user' | 'group' | 'region' | 'school' | 'departement';
   tags: TagsState;
   groups: GroupsState;
   users: UsersState;
