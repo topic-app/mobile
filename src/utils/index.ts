e29c9c24246d1d685e5708608d4da2f5b0d64785
Benjamin Sengupta <benjaminsengupta@gmail.com>
2020-12-07 21:39:21 +0100

    fix: bad rebase

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -5,6 +5,6 @@ export { default as Format } from './format';
 export * from './getAssetUrl';
 export * from './tags';
 export * from './hooks';
-export * from './groupPermissions';
+export * from './permissions';
 // Note: devicePermissions is for Permissions AND location functions
 export { Location } from './deviceLocation';
