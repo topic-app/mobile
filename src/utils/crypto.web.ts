09b0dad7f5fe070f65a9e85e051c4950e1c08b87
Alexander Nitters <alex@nitters.eu>
2020-12-11 14:04:38 +0100

    fix: fix ts

--- src/utils/crypto.web.ts
+++ src/utils/crypto.web.ts
@@ -1,17 +1,20 @@
+// @ts-nocheck
 import crypto from 'crypto-browserify';
 
 import { Config } from '@constants/index';
-import { logger } from '@utils';
 
 const hashPassword = (password: string) => {
   return new Promise((resolve, reject) => {
-    const hashedPassword = crypto.pbkdf2(
+    crypto.pbkdf2(
       Buffer.from(password),
       Buffer.from(Config.auth.salt),
       Config.auth.iterations,
       Config.auth.keylen,
       Config.auth.digest,
-      (err, derivedKey) => resolve(derivedKey.toString('base64')),
+      (err, derivedKey) => {
+        if (err) return reject();
+        resolve(derivedKey.toString('base64'));
+      },
     );
   });
 };
