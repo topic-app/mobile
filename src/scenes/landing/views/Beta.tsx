b4789d2856704ac5ce35ea6ca05f52b150d8cf04
Alex Nitters <alex@Rhonas-MacBook-Pro.local>
2020-12-19 12:04:47 +0100

    fix: fix wrong text in beta landing on ios

--- src/scenes/landing/views/Beta.tsx
+++ src/scenes/landing/views/Beta.tsx
@@ -192,9 +192,13 @@ const LandingArticles: React.FC<LandingArticlesProps> = ({ navigation }) => {
                         Les plantages seront reportés automatiquement, toutefois si vous constatez
                         un bug d&apos;affichage, une fonctionnalité qui ne marche pas correctement,
                         ou un autre problème, nous vous demandons de bien vouloir nous donner les
-                        détails.{'\n'}Vous pouvez faire cela en recherchant l&apos;application Topic
-                        sur le Play Store ou en cliquant sur &quot;Feedback&quot; dans le menu, et
-                        en cliquant sur &quot;envoyer des commentaires aux développeurs&quot;.{'\n'}
+                        détails.{'\n'}
+                        {Platform.OS === 'ios'
+                          ? 'Vous pouvez faire cela en passant par l’application Testflight et en cliquant sur "Envoyer des commentaires" ou en prenant une screenshot et en cliquant sur "Partager du feedback" après l’avoir annoté.'
+                          : `Vous pouvez faire cela en recherchant l’application Topic
+                        sur le Play Store ou en cliquant sur \"Feedback\" dans le menu, et
+                        en cliquant sur \"envoyer des commentaires aux développeurs\".`}
+                        {'\n'}
                       </Text>
                     </View>
                     <View style={landingStyles.contentContainer}>
@@ -286,7 +290,8 @@ const LandingArticles: React.FC<LandingArticlesProps> = ({ navigation }) => {
                     </View>
                     <View style={landingStyles.contentContainer}>
                       <Text>
-                        Nous publierons des mises à jour sur le Play Store toutes les semaines
+                        Nous publierons des mises à jour sur{' '}
+                        {Platform.OS === 'ios' ? 'Testflight' : 'le Play Store'} toutes les semaines
                         environ. N&apos;hésitez pas à regarder les notes de mise à jour pour voir
                         quelles fonctionnalités vous pouvez tester.{'\n'}
                       </Text>
