a161552d5c33bdb10ec2b894b047dadf54a4425a
Alexander Nitters <alex@nitters.eu>
2021-01-02 13:06:38 +0100

    fix: use a nicer login icon on web

--- src/scenes/auth/views/Login.tsx
+++ src/scenes/auth/views/Login.tsx
@@ -102,7 +102,11 @@ const AuthLogin: React.FC<AuthLoginProps> = ({
             <PlatformBackButton onPress={navigation.goBack} />
             <View style={authStyles.stepIndicatorContainer}>
               <View style={styles.centerIllustrationContainer}>
-                <Illustration name="auth-login" height={200} width={200} />
+                <Illustration
+                  name={Platform.OS === 'web' ? 'topic-icon-text' : 'auth-login'}
+                  height={200}
+                  width={200}
+                />
                 <Text style={authStyles.title}>Se connecter</Text>
               </View>
             </View>
