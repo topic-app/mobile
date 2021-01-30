e58abe29bfe5909f20b826b42a23000ec8874f15
Alex Nitters <alex@Rhonas-MBP.nitters.eu>
2021-01-30 12:59:25 +0100

    fix: fix modals etc on ios

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,27 +1,34 @@
 import React from 'react';
-import { Platform } from 'react-native';
+import { KeyboardAvoidingView, Platform, ScrollView, View } from 'react-native';
 import ModalComponent from 'react-native-modal';
 import { Card } from 'react-native-paper';
 
 import getStyles from '@styles/Styles';
 import { ModalProps } from '@ts/types';
-import { useTheme } from '@utils/index';
+import { useSafeAreaInsets, useTheme } from '@utils/index';
 
 const Modal: React.FC<ModalProps> = ({ visible, setVisible, children }) => {
   const styles = getStyles(useTheme());
 
   if (!visible && Platform.OS === 'ios') return null; // HACK: This is to fix a bug on iOS with multiple modals open
 
+  const insets = useSafeAreaInsets();
+
   return (
     <ModalComponent
       isVisible={visible}
       onBackButtonPress={() => setVisible(false)}
       onBackdropPress={() => setVisible(false)}
+      onSwipeComplete={() => setVisible(false)}
       useNativeDriver
       avoidKeyboard
       style={styles.bottomModal}
     >
-      <Card style={styles.modalCard}>{children}</Card>
+      <Card style={styles.modalCard}>
+        <ScrollView keyboardShouldPersistTaps="handled">
+          <View style={{ marginBottom: insets.bottom }}>{children}</View>
+        </ScrollView>
+      </Card>
     </ModalComponent>
   );
 };
