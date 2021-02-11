9d221ea0fc0bbed2ee13f07b7166687dc88137d3
Alexander Nitters <alex@nitters.eu>
2021-02-11 09:25:59 +0100

    feat: add custom tabs

--- src/scenes/more/about/components/SponsorsPage.tsx
+++ src/scenes/more/about/components/SponsorsPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, ScrollView, ActivityIndicator, Image } from 'react-native';
+import { View, ScrollView, ActivityIndicator, Image, TouchableOpacity } from 'react-native';
 import { Divider, Text } from 'react-native-paper';
 
 import { Content, ErrorMessage, PlatformTouchable } from '@components/index';
@@ -28,46 +28,46 @@ const SponsorsPage: React.FC = () => {
       </View>
       <View style={styles.contentContainer}>
         <Divider />
-        <PlatformTouchable onPress={() => handleUrl('https://mgen.fr')}>
-          <View style={[styles.centerImageContainer, { marginTop: 60, marginBottom: 10 }]}>
-            <Image style={{ height: 200 }} resizeMode="contain" source={image_mgen} />
-          </View>
-          <Text>
-            La Mutuelle générale de l&apos;Éducation nationale (MGEN) est l’entreprise qui finance
-            l’application, au niveau des frais de serveur, de publication et des frais associatifs.
-            Nous les remercions sincèrement pour cette aide qui nous permet de proposer une
-            application gratuite et sans publicité.
-          </Text>
-          <Text>
-            Plus d&apos;infos sur <Text style={styles.link}>mgen.fr</Text>
-          </Text>
-        </PlatformTouchable>
+        <View style={[styles.centerImageContainer, { marginTop: 60, marginBottom: 10 }]}>
+          <Image style={{ height: 200 }} resizeMode="contain" source={image_mgen} />
+        </View>
+        <Text>
+          La Mutuelle générale de l&apos;Éducation nationale (MGEN) est l’entreprise qui finance
+          l’application, au niveau des frais de serveur, de publication et des frais associatifs.
+          Nous les remercions sincèrement pour cette aide qui nous permet de proposer une
+          application gratuite et sans publicité.
+        </Text>
+        <Text>
+          Plus d&apos;infos sur{' '}
+          <TouchableOpacity onPress={() => handleUrl('https://mgen.fr')}>
+            <Text style={styles.link}>mgen.fr</Text>
+          </TouchableOpacity>
+        </Text>
         <Divider style={{ marginTop: 20 }} />
-        <PlatformTouchable onPress={() => handleUrl('https://edtechfrance.fr')}>
-          <View style={[styles.centerImageContainer, { marginTop: 60, marginBottom: 10 }]}>
-            <Image style={{ height: 200 }} resizeMode="contain" source={image_edtech} />
-          </View>
-          <Text>
-            L&apos;association EdTech France fédère les entreprises françaises qui ont décidé de
-            rendre la technologie et l&apos;innovation utiles à l&apos;éducation, à
-            l&apos;enseignement supérieur et à la formation tout au long de la vie.
-          </Text>
-          <Text>
-            Plus d&apos;infos sur <Text style={styles.link}>edtechfrance.fr</Text>
-          </Text>
-        </PlatformTouchable>
+        <View style={[styles.centerImageContainer, { marginTop: 60, marginBottom: 10 }]}>
+          <Image style={{ height: 200 }} resizeMode="contain" source={image_edtech} />
+        </View>
+        <Text>
+          L&apos;association EdTech France fédère les entreprises françaises qui ont décidé de
+          rendre la technologie et l&apos;innovation utiles à l&apos;éducation, à
+          l&apos;enseignement supérieur et à la formation tout au long de la vie.
+        </Text>
+        <Text>
+          Plus d&apos;infos sur{' '}
+          <TouchableOpacity onPress={() => handleUrl('https://edtechfrance.fr')}>
+            <Text style={styles.link}>edtechfrance.fr</Text>
+          </TouchableOpacity>
+        </Text>
         <Divider style={{ marginTop: 20 }} />
-        <PlatformTouchable onPress={() => handleUrl('https://instagram.com/jtacasso')}>
-          <View style={[styles.centerImageContainer, { marginTop: 60, marginBottom: 10 }]}>
-            <Image style={{ height: 200 }} resizeMode="contain" source={image_jtac} />
-          </View>
-          <Text>
-            La Jeunesse des Territoires pour l’Action Culturelle (JTAC) est une association dont le
-            but est de promouvoir l’accès à la culture et l’engagement lycéen. C’est une association
-            qui s’aligne parfaitement avec les buts de notre application, et nous avons donc décidé
-            de développer l’application en partenariat avec celle-ci.
-          </Text>
-        </PlatformTouchable>
+        <View style={[styles.centerImageContainer, { marginTop: 60, marginBottom: 10 }]}>
+          <Image style={{ height: 200 }} resizeMode="contain" source={image_jtac} />
+        </View>
+        <Text>
+          La Jeunesse des Territoires pour l’Action Culturelle (JTAC) est une association dont le
+          but est de promouvoir l’accès à la culture et l’engagement lycéen. C’est une association
+          qui s’aligne parfaitement avec les buts de notre application, et nous avons donc décidé de
+          développer l’application en partenariat avec celle-ci.
+        </Text>
         <Divider style={{ marginTop: 20 }} />
         <PlatformTouchable onPress={() => handleUrl('https://lesper.fr')}>
           <View style={[styles.centerImageContainer, { marginTop: 60, marginBottom: 10 }]}>
@@ -80,7 +80,10 @@ const SponsorsPage: React.FC = () => {
             l&apos;éducation.
           </Text>
           <Text>
-            Plus d&apos;infos sur <Text style={styles.link}>lesper.fr</Text>
+            Plus d&apos;infos sur{' '}
+            <TouchableOpacity onPress={() => handleUrl('https://lesper.fr')}>
+              <Text style={styles.link}>lesper.fr</Text>
+            </TouchableOpacity>
           </Text>
         </PlatformTouchable>
       </View>
