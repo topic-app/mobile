7bd98b25b68578ec497c85dd970a8586cd6f7633
Alexander Nitters <alex@nitters.eu>
2021-02-20 10:23:20 +0100

    fix: remove link to selectLocation in params

--- src/scenes/params/events/views/Params.tsx
+++ src/scenes/params/events/views/Params.tsx
@@ -93,11 +93,6 @@ const EventParamsScreen: React.FC<EventParamsProps> = ({
             <Text>Choisissez les écoles et départements dont vous voulez voir les évènements.</Text>
 
             <Text>Ces paramètres s&apos;appliquent aux évènements seulement. </Text>
-            <TouchableWithoutFeedback
-              onPress={() => navigation.navigate('Landing', { screen: 'SelectLocation' })}
-            >
-              <Text style={[styles.link, styles.primaryText]}>Changer d&apos;école</Text>
-            </TouchableWithoutFeedback>
           </View>
         </View>
         <Divider />
