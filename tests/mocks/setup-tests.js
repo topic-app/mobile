d1a55f5322c0131532f3c7f10d9c1ecaad0f946a
Alexander Nitters <alex@nitters.eu>
2021-03-20 12:41:49 +0100

    fix: fix unit tests

--- tests/mocks/setup-tests.js
+++ tests/mocks/setup-tests.js
@@ -1,8 +1,10 @@
 import mockRNDeviceInfo from 'react-native-device-info/jest/react-native-device-info-mock';
 
 import mockAsyncStorage from './async-storage';
+import mockFirebase from './firebase';
 import mockRNGeolocationService from './react-native-geolocation-service';
 
 jest.mock('react-native-device-info', () => mockRNDeviceInfo);
 jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
 jest.mock('react-native-geolocation-service', () => mockRNGeolocationService);
+jest.mock('@utils/firebase', () => mockFirebase);
