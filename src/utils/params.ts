67e91c124b94b0c15c1643ea029d3969ac4a1124
Alexander Nitters <alex@nitters.eu>
2021-04-08 21:53:55 +0200

    fix: fix eslint error

--- src/utils/params.ts
+++ src/utils/params.ts
@@ -2,7 +2,7 @@ import Store from '@redux/store';
 import { ReduxLocation } from '@ts/types';
 
 function getContentParams(): ReduxLocation {
-  const location = Store.getState().location;
+  const { location } = Store.getState();
   // The filter is normally not necessary but we seem to frequently have problems with the data
   return {
     schools: location.schools.filter((s) => !!s),
