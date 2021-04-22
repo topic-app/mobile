a4f7d72b8d93ef2e3f1f838ee2bbddfddf1debe8
Alexander Nitters <alex@nitters.eu>
2021-04-22 13:13:27 +0200

    fix: revert to react-native-screens 3.0.0 to avoid transition bugs

--- App.tsx
+++ App.tsx
@@ -3,15 +3,12 @@ import 'moment/locale/fr';
 import React from 'react';
 import 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { enableScreens } from 'react-native-screens';
 import { Provider as ReduxProvider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
 import Store, { Persistor } from '@redux/store';
 
 import StoreApp from './src/StoreApp';
-
-enableScreens();
 
 function App() {
   return (
