723d4768228703cfe8c2841e483b7c5249fed843
Benjamin Sengupta <benjaminsengupta@gmail.com>
2021-04-30 21:21:26 +0200

    feat: use react-spring for animation on web welcome

--- src/scenes/landing/Welcome.web.tsx
+++ src/scenes/landing/Welcome.web.tsx
@@ -1,14 +1,54 @@
-import { values } from 'lodash';
-import React from 'react';
-import { View, Platform } from 'react-native';
+import { throttle } from 'lodash';
+import React, { useCallback, useState } from 'react';
+import { View, ScrollView } from 'react-native';
 import { Divider, Text, Title, Button, useTheme, Searchbar } from 'react-native-paper';
+import { animated, useTransition } from 'react-spring';
 
-import { Illustration, TranslucentStatusBar } from '@components';
+import { Illustration } from '@components';
+import { IllustrationName } from '@components/Illustration';
 import { updateDepartments } from '@redux/actions/api/departments';
 import { trackEvent } from '@utils';
 
 import type { LandingScreenNavigationProp } from '.';
 import getStyles from './styles';
+
+type TileProps = {
+  illustration: IllustrationName;
+  text: string;
+  onPress: () => any;
+};
+
+const Tile: React.FC<TileProps> = ({ illustration, text, onPress }) => {
+  // val -> (val+1)%360 val = 0|1|2|3|...|358|359|0|1;
+  // Math.abs(tilenumber-val)<360/4|>3*360/4
+  return (
+    <Button onPress={() => onPress()}>
+      <View>
+        <Illustration name={illustration} />
+        <Text style={{ textAlign: 'center' }}>{text}</Text>
+      </View>
+    </Button>
+  );
+};
+
+const tileData: { text: string; illustration: IllustrationName }[] = [
+  {
+    text: 'hello 1',
+    illustration: 'event',
+  },
+  {
+    text: 'hello 2',
+    illustration: 'article',
+  },
+  {
+    text: 'hello 3',
+    illustration: 'auth-login',
+  },
+  {
+    text: 'hello 4',
+    illustration: 'beta-messages',
+  },
+];
 
 type LandingWelcomeProps = {
   navigation: LandingScreenNavigationProp<'Welcome'>;
@@ -16,6 +56,7 @@ type LandingWelcomeProps = {
 
 const LandingWelcome: React.FC<LandingWelcomeProps> = ({ navigation }) => {
   const theme = useTheme();
+  const { colors } = theme;
   const styles = getStyles(theme);
 
   React.useEffect(() => {
@@ -24,102 +65,39 @@ const LandingWelcome: React.FC<LandingWelcomeProps> = ({ navigation }) => {
     trackEvent('firstopen');
   }, []);
 
-  const Animation: (
-    start: number,
-    setV: (i: number) => void,
-    v: number,
-  ) => {
-    input: number;
-    value: number;
-    setVal: (t: number) => void;
-    bool: boolean;
-  } = (start, setV, v) => {
-    return {
-      input: start,
-      value: v,
-      setVal: setV,
-      bool: !0,
-    };
-  };
+  const [index, setIndex] = useState(0);
 
-  const startAnimation: (
-    e: {
-      value: number;
-      setVal: (t: number) => void;
-      bool: boolean;
-    },
-    stop: number,
-    step: number | ((i: number) => number),
-  ) => void = (e, stop, step) => {
-    e.setVal(typeof step === 'number' ? (e.value += step) : (e.value = step(e.value)));
-    // console.log(e.value, Math.round(e.value * 1e3) / 1e3, stop);
-    if (e.bool) {
-      if (Math.round(e.value * 1e2) / 1e2 !== stop) {
-        requestAnimationFrame(() => {
-          startAnimation(e, stop, step);
-        });
-      }
-    } else e.bool = true;
-  };
+  const handlePrev = useCallback(
+    throttle(() => setIndex((state) => (state - 1) % tileData.length), 500),
+    [setIndex],
+  );
 
-  const [val, setV] = React.useState(0);
-  const valchange = Animation(0, setV, val);
+  const handleNext = useCallback(
+    throttle(() => setIndex((state) => (state + 1) % tileData.length), 500),
+    [setIndex],
+  );
 
-  const moveLeft: () => void = () => {
-    startAnimation(valchange, (valchange.value + 3) % 4, (i) => (i - 1e-2 + 4) % 4);
-  };
-
-  const moveRight: () => void = () => {
-    console.log('hi');
-    startAnimation(valchange, (valchange.value + 1) % 4, (i) => (i + 1e-2) % 4);
-  };
-
-  type TileProps = {
-    text?: string;
-    tilenumber?: number;
-    name: 'article' | 'event' | 'explore' | 'group';
-    onPress?: () => void;
-  };
-
-  const Tile: React.FC<TileProps> = ({ text = '', tilenumber = 0, name, onPress }) => {
-    // val -> (val+1)%360 val = 0|1|2|3|...|358|359|0|1;
-    // Math.abs(tilenumber-val)<360/4|>3*360/4
-    return (
-      <Button
-        onPress={() => moveRight()}
-        style={{
-          position: tilenumber === 0 ? 'relative' : 'absolute',
-          left: ((val + ((tilenumber + 2) % 4) - 2) * window.innerWidth) / 2,
-        }}
-      >
-        <View
-        // style={{
-        //   translateX: 200, // ((val + ((tilenumber + 2) % 4) - 2) * window.innerWidth) / 2,
-        // }}
-        >
-          <Illustration name={name} />
-          <Text style={{ textAlign: 'center' }}>{text}</Text>
-        </View>
-      </Button>
-    );
-  };
+  const transitions = useTransition([tileData[index]], {
+    from: { opacity: 0, transform: 'translateX(-100px)' },
+    enter: { opacity: 1, transform: 'translateX(0px)' },
+    leave: { opacity: 0, transform: 'translateX(100px)' },
+    delay: 1500,
+  });
 
   return (
-    <View style={styles.page}>
-      <TranslucentStatusBar />
-      <View>
-        <View style={{ flexDirection: 'row', flex: 1 }}>
+    <View style={{ backgroundColor: colors.background, width: '100vw', height: '100vh' }}>
+      <View style={{ width: '100%', height: '80px', backgroundColor: colors.appBar }}>
+        <View style={{ flexDirection: 'row', flex: 1, alignItems: 'center' }}>
           <View style={{ margin: 10 }}>
-            <Illustration name="topic-icon" style={{ height: 50, width: 50 }} />
+            <Illustration name="topic-icon" height={50} width={50} />
           </View>
-          <Text style={[styles.title, { fontSize: 30, marginTop: 15 }]}>Topic</Text>
+          <Text style={[styles.title, { fontSize: 30 }]}>Topic</Text>
           <View
-            style={[{ flexDirection: 'row', flex: 1, justifyContent: 'flex-end', marginTop: 10 }]}
+            style={[{ flexDirection: 'row', flex: 1, justifyContent: 'flex-end', marginRight: 10 }]}
           >
             <Button
               style={{ height: 35, margin: 5 }}
-              mode="contained"
-              uppercase={Platform.OS !== 'ios'}
+              mode="outlined"
               onPress={() =>
                 navigation.navigate('Auth', {
                   screen: 'Login',
@@ -131,7 +109,6 @@ const LandingWelcome: React.FC<LandingWelcomeProps> = ({ navigation }) => {
             <Button
               style={{ height: 35, margin: 5, marginRight: 10 }}
               mode="contained"
-              uppercase={Platform.OS !== 'ios'}
               onPress={() => {
                 navigation.navigate('Beta');
               }}
@@ -140,48 +117,57 @@ const LandingWelcome: React.FC<LandingWelcomeProps> = ({ navigation }) => {
             </Button>
           </View>
         </View>
-      </View>
-      <View>
         <Divider />
-        <View style={{ display: 'flex', flexWrap: 'wrap', height: 'calc(100% - 50px)' }}>
-          <View
-            style={{
-              width: '50vw',
-              height: '100%',
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}
-          >
-            <Title style={[styles.title, { fontSize: 30, alignSelf: 'center', marginTop: 20 }]}>
-              Choisissez votre école
-            </Title>
-            <View style={{ width: 400, marginVertical: 20, alignSelf: 'center' }}>
-              <Searchbar
-                placeholder="Rechercher"
-                onChangeText={() => {
-                  console.log(this);
-                }}
-              />
+      </View>
+      <ScrollView>
+        <View style={{ height: 'calc(100vh - 80px)', width: '100%' }}>
+          <View style={{ flex: 1, flexDirection: 'row' }}>
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+              <Title style={[styles.title, { fontSize: 30, marginTop: 20 }]}>
+                Choisissez votre école
+              </Title>
+              <View style={{ width: 400, marginVertical: 20 }}>
+                <Searchbar
+                  placeholder="Rechercher"
+                  onChangeText={() => {
+                    console.log(this);
+                  }}
+                />
+              </View>
+            </View>
+            <View
+              style={{
+                flex: 1,
+                alignItems: 'center',
+                justifyContent: 'center',
+                overflow: 'hidden',
+              }}
+            >
+              {transitions(({ opacity, transform }, props) => (
+                <animated.div
+                  style={{
+                    opacity,
+                    transform,
+                    position: 'absolute',
+                  }}
+                >
+                  <Tile {...props} onPress={handleNext} />
+                </animated.div>
+              ))}
             </View>
           </View>
-          <View
-            style={{
-              width: '50vw',
-              height: '100%',
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-              overflow: 'hidden',
-            }}
-          >
-            <Tile text="hello" name="event" tilenumber={0} onPress={moveRight} />
-            <Tile text="hi" name="article" tilenumber={1} onPress={moveRight} />
-            <Tile text="world" name="article" tilenumber={2} onPress={moveRight} />
-            <Tile text="mushroom" name="article" tilenumber={3} onPress={moveRight} />
-          </View>
         </View>
-      </View>
+        <View
+          style={{
+            height: 'calc(100vh - 80px)',
+            width: '100%',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <Text>Page 2!</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 };
