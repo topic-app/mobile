712807f44b337c6a7832798998050b9c2a3e9a0d
Joachim Laplanche <joachim.laplanche@gmail.com>
2021-04-30 16:30:31 +0000

    feat: improve welcome screen

--- src/scenes/landing/Welcome.web.tsx
+++ src/scenes/landing/Welcome.web.tsx
@@ -1,3 +1,4 @@
+import { values } from 'lodash';
 import React from 'react';
 import { View, Platform } from 'react-native';
 import { Divider, Text, Title, Button, useTheme, Searchbar } from 'react-native-paper';
@@ -22,6 +23,86 @@ const LandingWelcome: React.FC<LandingWelcomeProps> = ({ navigation }) => {
     updateDepartments('initial');
     trackEvent('firstopen');
   }, []);
+
+  const Animation: (
+    start: number,
+    setV: (i: number) => void,
+    v: number,
+  ) => {
+    input: number;
+    value: number;
+    setVal: (t: number) => void;
+    bool: boolean;
+  } = (start, setV, v) => {
+    return {
+      input: start,
+      value: v,
+      setVal: setV,
+      bool: !0,
+    };
+  };
+
+  const startAnimation: (
+    e: {
+      value: number;
+      setVal: (t: number) => void;
+      bool: boolean;
+    },
+    stop: number,
+    step: number | ((i: number) => number),
+  ) => void = (e, stop, step) => {
+    e.setVal(typeof step === 'number' ? (e.value += step) : (e.value = step(e.value)));
+    // console.log(e.value, Math.round(e.value * 1e3) / 1e3, stop);
+    if (e.bool) {
+      if (Math.round(e.value * 1e2) / 1e2 !== stop) {
+        requestAnimationFrame(() => {
+          startAnimation(e, stop, step);
+        });
+      }
+    } else e.bool = true;
+  };
+
+  const [val, setV] = React.useState(0);
+  const valchange = Animation(0, setV, val);
+
+  const moveLeft: () => void = () => {
+    startAnimation(valchange, (valchange.value + 3) % 4, (i) => (i - 1e-2 + 4) % 4);
+  };
+
+  const moveRight: () => void = () => {
+    console.log('hi');
+    startAnimation(valchange, (valchange.value + 1) % 4, (i) => (i + 1e-2) % 4);
+  };
+
+  type TileProps = {
+    text?: string;
+    tilenumber?: number;
+    name: 'article' | 'event' | 'explore' | 'group';
+    onPress?: () => void;
+  };
+
+  const Tile: React.FC<TileProps> = ({ text = '', tilenumber = 0, name, onPress }) => {
+    // val -> (val+1)%360 val = 0|1|2|3|...|358|359|0|1;
+    // Math.abs(tilenumber-val)<360/4|>3*360/4
+    return (
+      <Button
+        onPress={() => moveRight()}
+        style={{
+          position: tilenumber === 0 ? 'relative' : 'absolute',
+          left: ((val + ((tilenumber + 2) % 4) - 2) * window.innerWidth) / 2,
+        }}
+      >
+        <View
+        // style={{
+        //   translateX: 200, // ((val + ((tilenumber + 2) % 4) - 2) * window.innerWidth) / 2,
+        // }}
+        >
+          <Illustration name={name} />
+          <Text style={{ textAlign: 'center' }}>{text}</Text>
+        </View>
+      </Button>
+    );
+  };
 
   return (
     <View style={styles.page}>
@@ -62,11 +143,43 @@ const LandingWelcome: React.FC<LandingWelcomeProps> = ({ navigation }) => {
       </View>
       <View>
         <Divider />
-        <Title style={[styles.title, { fontSize: 30, alignSelf: 'center', marginTop: 20 }]}>
-          Choisissez votre école
-        </Title>
-        <View style={{ width: 400, marginVertical: 20, alignSelf: 'center' }}>
-          <Searchbar placeholder="Rechercher" />
+        <View style={{ display: 'flex', flexWrap: 'wrap', height: 'calc(100% - 50px)' }}>
+          <View
+            style={{
+              width: '50vw',
+              height: '100%',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <Title style={[styles.title, { fontSize: 30, alignSelf: 'center', marginTop: 20 }]}>
+              Choisissez votre école
+            </Title>
+            <View style={{ width: 400, marginVertical: 20, alignSelf: 'center' }}>
+              <Searchbar
+                placeholder="Rechercher"
+                onChangeText={() => {
+                  console.log(this);
+                }}
+              />
+            </View>
+          </View>
+          <View
+            style={{
+              width: '50vw',
+              height: '100%',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              overflow: 'hidden',
+            }}
+          >
+            <Tile text="hello" name="event" tilenumber={0} onPress={moveRight} />
+            <Tile text="hi" name="article" tilenumber={1} onPress={moveRight} />
+            <Tile text="world" name="article" tilenumber={2} onPress={moveRight} />
+            <Tile text="mushroom" name="article" tilenumber={3} onPress={moveRight} />
+          </View>
         </View>
       </View>
     </View>
